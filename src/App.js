7c264014e1d506bc863426dfe678c06b2eea7a28
abirhasan33 <abir03512@gmail.com>
2022-03-16 20:58:35 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Abir hasan</h2>
+      <Counter></Counter>
+      <LodeCommrnts></LodeCommrnts>
     </div>
   );
+}
+
+function Counter(){
+  const [count, setCount] = useState(0);
+
+  const handeleIncrase = () => setCount(count + 100);
+  const handeleDecrase = () => setCount(count - 100);
+
+  return(
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={handeleIncrase}>Inctease</button>
+      <button onClick={handeleDecrase}>Dectease</button>
+    </div>
+  )
+}
+
+function LodeCommrnts() {
+  const [comments, setCommites] = useState([]);
+
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(res => res.json())
+    .then(data => setCommites(data))
+  },[])
+
+  return(
+    <div>
+      <h2>Commit: {comments.length}</h2>
+      {
+        comments.map(comment => <Commit email={comment.email} body={comment.body} id={comment.id} name={comment.name}></Commit>)
+      }
+    </div>
+  )
+}
+
+function Commit (props){
+  return(
+    <div>
+      <h2>Name: {props.name}</h2>
+      <h4>Email: {props.email}</h4>
+      <h5>Body: {props.body}</h5>
+      <h4>Id: {props.id}</h4>
+    </div>
+  )
 }
 
 export default App;
